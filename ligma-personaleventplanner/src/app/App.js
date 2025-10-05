6f480ee4c2cb0420bc9dfaf2b629f62baf028e4a
Exanperanto605 <147262194+Exanperanto605@users.noreply.github.com>
2025-10-05 21:31:02 +0700

    401 Error & Commect Editing

---
+++ ligma-personaleventplanner/src/app/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { signInWithPopup } from 'firebase/auth';
+import { auth, googleAuthProvider } from '../firebaseConfig';
+import { unauthorized } from 'next/navigation'
+
+function App() {
+    const loginWithGoogle = async () => {
+        try {
+            const result = await signInWithPopup(auth, googleAuthProvider);
+            const user = result.user;
+            alert(`Welcome ${user.displayName}`);   // Placeholder
+        } catch (error) {
+            // alert(error.message);   // Placeholder
+            unauthorized();     // Redirect to 401
+        }
+    };
+
+    return {
+        // Login UI WIP
+    }
+}
+
+export default App;
