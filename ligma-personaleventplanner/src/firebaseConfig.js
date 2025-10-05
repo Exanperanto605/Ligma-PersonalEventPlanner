3a5c5ffe4e464ee9a6e8279ce2ad7a3ea145b67e
Exanperanto605 <147262194+Exanperanto605@users.noreply.github.com>
2025-10-05 14:26:16 +0700

    User Auth: Create 'firebaseConfig'

---
+++ ligma-personaleventplanner/src/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATu0GiahKhqmpyTaxUo1uSqsO0DppghGg",
+    authDomain: "ligma-personaleventplanner.firebaseapp.com",
+    projectId: "ligma-personaleventplanner",
+    storageBucket: "ligma-personaleventplanner.firebasestorage.app",
+    messagingSenderId: "66250269201",
+    appId: "1:66250269201:web:c13ce91a58ae0d50f647b0",
+    measurementId: "G-82BB3BME5H"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const googleAuthProvider = new GoogleAuthProvider();
+
+export { auth, googleAuthProvider };
