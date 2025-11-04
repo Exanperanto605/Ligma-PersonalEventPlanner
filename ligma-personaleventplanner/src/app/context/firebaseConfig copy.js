86c08ca96e964cfdad3424cfe1ca7b91c751349a
Exanperanto605 <147262194+Exanperanto605@users.noreply.github.com>
2025-11-04 16:32:40 +0700

    Duplicate 'firebaseConfig' and 'AuthProvider' files for email/password sign-in and sign-up implemention.

---
+++ ligma-personaleventplanner/src/app/context/firebaseConfig copy.js
@@ -0,0 +1,26 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATu0GiahKhqmpyTaxUo1uSqsO0DppghGg",
+    authDomain: "ligma-personaleventplanner.firebaseapp.com",
+    projectId: "ligma-personaleventplanner",
+    storageBucket: "ligma-personaleventplanner.firebasestorage.app",
+    messagingSenderId: "66250269201",
+    appId: "1:66250269201:web:c13ce91a58ae0d50f647b0",
+    measurementId: "G-82BB3BME5H"
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const auth = getAuth(app);
+
+const googleAuthProvider = new GoogleAuthProvider();
+// Ensure we receive basic profile info including photoURL
+googleAuthProvider.addScope('profile');
+googleAuthProvider.addScope('email');
+googleAuthProvider.setCustomParameters({ prompt: 'select_account' });
+
+export { app, auth, googleAuthProvider };
+export const db = getFirestore(app);
