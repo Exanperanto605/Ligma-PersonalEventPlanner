d7a3a060309df07d8313a0dd7f0cb0a9fbf3b5b8
Exanperanto605 <147262194+Exanperanto605@users.noreply.github.com>
2025-11-06 20:56:50 +0700

    create-user page stylesheet + UI

--- ligma-personaleventplanner/src/app/auth/create-user/page.jsx
+++ ligma-personaleventplanner/src/app/auth/create-user/page.jsx
@@ -1,8 +1,7 @@
 import { useContext, useState } from "react";
 import { UserAuthContext } from "../../context/auth-context";
-import { db } from "../../context/firebaseConfig";
-import { doc, setDoc } from "firebase/firestore";
 import { useRouter } from "next/navigation";
+import styles from "./styles/create-user_style.module.css";
 
 function CreateNewAccount() {
     const { signUpWithEmailPW } = useContext(UserAuthContext);
@@ -12,11 +11,19 @@ function CreateNewAccount() {
     const [username, setUsername] = useState("");
     const [dateOfBirth, setDateOfBirth] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     const [error, setError] = useState("");
 
     const handleSignUpWithEmailPW = async (e) => {
         e.preventDefault();
+        setError("");
+
+        if (password !== confirmPassword) {
+            setError("Passwords do not match. Please try again.");
+            return;
+        }
+
         try {
             const userCred = signUpWithEmailPW(email, username, dateOfBirth, password);
             router.push('/calendar/view')
@@ -25,8 +32,9 @@ function CreateNewAccount() {
         }
     }
 
-    const handleBrithdateChange = (e) => {
+    const handleBirthdateChange = (e) => {
         const value = e.target.value;
+        setError("");
 
         if (!isValidBirthdateFormat(value)) {
             setError("Please enter a valid date in DD/MM/YYYY format.");
@@ -57,6 +65,87 @@ function CreateNewAccount() {
     }
     
     return (
-        // HTML Markup
+        <div>
+            <div className={styles.card}>
+                <h1 className={styles.nameh1}>Create your account</h1>
+                <form onSubmit={handleSignUpWithEmailPW}>
+                    <div className={styles.inputgroup}>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="something@email.smth"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    </div>
+
+                    <div className={styles.inputgroup}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        id="username"
+                        type="username"
+                        placeholder="JohnDoe123"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                    </div>
+
+                    <div className={styles.inputgroup}>
+                    <label htmlFor="username">Birth Date</label>
+                    <input
+                        id="birthdate"
+                        type="birthdate"
+                        placeholder="01/01/1990"
+                        value={dateOfBirth}
+                        onChange={(e) => handleBirthdateChange(e)}
+                        required
+                    />
+                    </div>
+
+                    <div className={styles.inputgroup}>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    </div>
+
+                    <div className={styles.inputgroup}>
+                    <label htmlFor="confirm-password">Confirm Password</label>
+                    <input
+                        id="confirm-password"
+                        type="confirm-password"
+                        placeholder="Password"
+                        value={confirmPassword && (
+                                <p
+                                    className={`text-sm ${
+                                    password === confirmPassword ? "text-green-600" : "text-red-600"
+                                    }`}
+                                >
+                                    {password === confirmPassword ? "Passwords match. All good! :)" : "Both passwords must match. :("}
+                                </p>
+                                )}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                    />
+                    </div>
+
+                    <button className={styles.submitEmail} type="submit">
+                    Submit
+                    </button>
+
+                    <div className={styles.links}>
+                    <a href="/">Already have an account?</a>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
