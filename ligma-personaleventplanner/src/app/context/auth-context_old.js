4e2319cbb2b605140ec862ca4581de8f3ef1ab3e
Exanperanto605 <147262194+Exanperanto605@users.noreply.github.com>
2025-11-09 23:36:21 +0700

    Removing old files.

--- ligma-personaleventplanner/src/app/context/auth-context_old.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const initialValue = {
-    user: null,
-    signInWithGoogle: () => {},
-    signOut: () => {},
-};
-
-export const UserAuthContext = createContext(initialValue);
